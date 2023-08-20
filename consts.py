faf25e5af8b63cf0e1a86b15a9732398d90e5174
GiladBach <52894908+GiladBach@users.noreply.github.com>
2023-08-20 14:44:49 +0300

    Delete consts.py

--- consts.py
+++
@@ -1,7 +0,0 @@
-the_weather_channel_url = 'https://weather.com/he-IL/weather/today/l/31.90,35.20?par=google'
-
-moon_phases_url = 'https://www.astropro.co.il/moon-phase?dt='
-
-time_genie_url = 'https://heb.timegenie.com/amp/salah/city/psrmh'
-
-headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}
