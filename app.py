e17d5cfc76fc52402ad07b3e52d63f92089d4bb6
GiladBach <gilad.bach1470@gmail.com>
2023-09-06 22:54:59 +0300

    yes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import requests
 from flask import Flask, render_template, request, redirect, url_for
 from consts import API_KEY as api_key
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,7 +10,6 @@ def get_weather_data(city, date):
     base_url = f'http://api.weatherapi.com/v1/history.json?key={api_key}&q={city}&dt={date}'
     response = requests.get(base_url)
     data = response.json()
-    print(data)
     return data
 
 #gets the prayer times
@@ -17,7 +17,6 @@ def get_prayer_times(city, date):
     base_url = f'http://api.aladhan.com/v1/timingsByCity?city={city}&country=CountryName&date={date}&method=2'
     response = requests.get(base_url)
     data = response.json()
-    print(data)
     return data
 
 @app.route('/', methods=['GET', 'POST'])
@@ -33,13 +32,29 @@ def index():
 
 @app.route('/response/<city>/<date>')
 def response(city, date):
-    if request.remote_addr:
-        print('id: ' + str(request.remote_addr))
-    else:
-        print('no ip? wtf')
+    # if request.remote_addr:
+    #     print('id: ' + str(request.remote_addr))
+    # else:
+    #     print('no ip? wtf')
     
     weather_data = get_weather_data(city, date)
     prayer_times = get_prayer_times(city, date)
+    formatted_datetime = datetime.now().strftime('%d/%m/%Y %H:%M')
+    required_data = {
+        'Time': formatted_datetime,
+        'High': weather_data['forecast']['forecastday'][0]['day']['maxtemp_c'],
+        'Low': weather_data['forecast']['forecastday'][0]['day']['mintemp_c'],
+        'Sunrise': weather_data['forecast']['forecastday'][0]['astro']['sunrise'],
+        'Sunset': weather_data['forecast']['forecastday'][0]['astro']['sunset'],
+        'Condition': weather_data['forecast']['forecastday'][0]['day']['condition']['text'],
+        'Prec': weather_data['forecast']['forecastday'][0]['day']['totalprecip_mm'],
+        'Moon': weather_data['forecast']['forecastday'][0]['astro']['moon_illumination'],
+        'Date': weather_data['forecast']['forecastday'][0]['date'],
+        'Fajr': prayer_times['data']['timings']['Fajr'],
+        'IP': str(request.remote_addr) if request.remote_addr else 'idk'
+    }
+
+    print(', '.join([str(x) + ': ' + str(y) for x, y in required_data.items()]))
     return render_template('response.html', weather_data=weather_data, prayer_times=prayer_times)
 
 if __name__ == '__main__':
